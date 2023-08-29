987fbd88b34b8210f4464d342a0eb30cf893d42f
Wouter Scherphof <wouter.scherphof@gmail.com>
2023-08-29 11:00:46 +0200

    fix call to save_additional_libs.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-set -e
 
 docker image build \
 	--build-arg DOCKER_USER="$DOCKER_USER" \
 	--build-arg STABLE_EXTENSIONS="$STABLE_EXTENSIONS" \
 	--build-arg COMMUNITY_EXTENSIONS="$COMMUNITY_EXTENSIONS" \
-	-t "$IMAGE" .
-
-docker container run --rm \
-	--mount src="$(realpath conf/additional_libs)",target=/save_additional_libs/,type=bind \
-	docker4gis/geoserver \
-	/opt/additional_libs/save_additional_libs.sh
+	-t "$IMAGE" . &&
+	docker container run --rm \
+		--mount src="$(realpath conf/additional_libs)",target=/save_additional_libs/,type=bind \
+		"$DOCKER_REGISTRY"/"$DOCKER_USER"/"$DOCKER_REPO" \
+		/opt/additional_libs/save_additional_libs.sh
