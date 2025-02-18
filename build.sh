a0d613c620432a79f7d09f54463c7dc15d09a978
Azure Pipeline <pipeline@azure.com>
2025-02-18 10:21:08 +0100

    Postgis JNDI store

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ docker image build \
 	--build-arg DOCKER_REPO="$DOCKER_REPO" \
 	--build-arg STABLE_EXTENSIONS="$STABLE_EXTENSIONS" \
 	--build-arg COMMUNITY_EXTENSIONS="$COMMUNITY_EXTENSIONS" \
+	--build-arg POSTGRES_HOST="$POSTGRES_HOST" \
+	--build-arg POSTGRES_PORT="$POSTGRES_PORT" \
+	--build-arg POSTGRES_DB="$POSTGRES_DB" \
+	--build-arg POSTGRES_USERNAME="$POSTGRES_USERNAME" \
+	--build-arg POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
 	-t "$DOCKER_IMAGE" . &&
 	docker container run --rm \
 		--mount src="$(realpath conf/additional_libs)",target=/save_additional_libs/,type=bind \
